cffec30f58e3926f488f7d45dee5b59f51499f52
Jordan Walker <jwalker117@gmail.com>
2020-03-16 21:58:55 -0500

    Fixed Order of operation on MathParser

--- server/modules/mathParser.js
+++ server/modules/mathParser.js
@@ -3,41 +3,35 @@ let parseEquation = (array) => {
 	let mappedArray = array.map(item => item);
 	
 	//Order of Operations Multiply, Divide, Addition, Subtraction
-	//Check for Multiplication
+	
+	//Check for Multiplication or Division
 	for (let i = 0; i < mappedArray.length; i++) {
-		if(mappedArray[i] === '*') {
-			let answer =  Number(mappedArray[i - 1]) * Number(mappedArray[i + 1]);
-			mappedArray.splice(i-1,3, answer);
-			i--;
-		}
+		if (mappedArray[i] === '*'|| mappedArray[i] === '/') {
+			if(mappedArray[i] === '*') {
+				let answer =  Number(mappedArray[i - 1]) * Number(mappedArray[i + 1]);
+				mappedArray.splice(i-1,3, answer);
+			} else if(mappedArray[i] === '/') {
+				let answer =  Number(mappedArray[i - 1]) / Number(mappedArray[i + 1]);
+				mappedArray.splice(i-1,3, answer);
+			}
+		i--; //setback index back by 1
+		}	
 	}
 
-	//Check for Division
+	//Check for Addition or Subtraction
 	for (let i = 0; i < mappedArray.length; i++) {
-		if(mappedArray[i] === '/') {
-			let answer =  Number(mappedArray[i - 1]) / Number(mappedArray[i + 1]);
-			mappedArray.splice(i-1,3, answer);
-			i--;
-		}
+		if (mappedArray[i] === '+'|| mappedArray[i] === '-') {
+			if(mappedArray[i] === '+') {
+				let answer =  Number(mappedArray[i - 1]) + Number(mappedArray[i + 1]);
+				mappedArray.splice(i-1,3, answer);
+			} else if(mappedArray[i] === '-') {
+				let answer =  Number(mappedArray[i - 1]) - Number(mappedArray[i + 1]);
+				mappedArray.splice(i-1,3, answer);
+			}
+		i--; //setback index back by 1
+		}	
 	}
 
-	//Check for Addition
-	for (let i = 0; i < mappedArray.length; i++) {
-		if(mappedArray[i] === '+') {
-			let answer =  Number(mappedArray[i - 1]) + Number(mappedArray[i + 1]);
-			mappedArray.splice(i-1,3, answer);
-			i--;
-		}
-	}
-
-	//Check for Subtraction
-	for (let i = 0; i < mappedArray.length; i++) {
-		if(mappedArray[i] === '-') {
-			let answer =  Number(mappedArray[i - 1]) - Number(mappedArray[i + 1]);
-			mappedArray.splice(i-1,3, answer);
-			i--;
-		}
-	}
 	//Create an object to push into array at the end
 	let objectAnswer = {answer: mappedArray[0]};
 	array.push(objectAnswer);
